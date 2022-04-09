60cedc843a943f4591101e80f6392c0229efc8f2
Martin Reece <martin@Martins-Air.home>
2022-04-09 17:02:45 -0400

    I created three variables with comments inside my JavaScript file

---
+++ verb.js
@@ -0,0 +1,9 @@
+// This is where I'm picking out all the elements with ids on them
+// Create a variable name "computerChoiceDisplay" and grab the id name of my <span> tag ("computer-choice") from the DOM by using the document.getElementById() method. I can use this to read or edit(modify) an HTML element
+const computerChoiceDisplay = document.getElementById('computer-choice');
+
+// Create a variable name "userChoiceDisplay" and grab the id name of my <span> tag ("user-choice") from the DOM by using the document.getElementById() method. I can use this to read or edit(modify) an HTML element
+const userChoiceDisplay = document.getElementById('user-choice');
+
+// Create a variable name "resultDisplay" and grab the id name of my <span> tag ("result") from the DOM by using the document.getElementById() method. I can use this to read or edit(modify) an HTML element
+const resultDisplay = document.getElementById('result');
